b6d69fe729ec479531dd4be01e37a5142b68d353
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-12-13 21:54:23 +0200

    Task 6 Done

--- task_2/js/main.ts
+++ task_2/js/main.ts
@@ -44,3 +44,15 @@ function createEmployee(salary: number | string): Director | Teacher {
   }
   return new Director();
 }
+
+function isDirector(employee: Teacher | Director): employee is Director {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Teacher | Director) {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
