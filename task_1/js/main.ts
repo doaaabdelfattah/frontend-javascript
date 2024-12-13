f95ac376e3e769bfa3971a81d7e2fb35d070db59
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-12-13 03:27:55 +0200

    Task 3

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -1,3 +1,4 @@
+// ============= I N T E R F A C E S =============
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
@@ -7,3 +8,19 @@ interface Teacher {
   contract: boolean;
   [key: string]: any;
 }
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// ============== F U N C T I O N S =============
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+// ============== C L A S S E S ================
